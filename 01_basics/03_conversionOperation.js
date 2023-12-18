f517a3eaa63198e552a9add7e0800af257c5b49b
adityatejasvi <111077816+adityatejasvi@users.noreply.github.com>
2023-12-18 09:07:21 +0000

    typeConversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score =33
+
+console.log(typeof score);
+
+score="33"
+console.log(typeof score)
+
+// here we can convert like this too 
+let age="20"
+
+console.log(typeof age)
+
+let ageInNumber=Number(age)
+console.log(ageInNumber)
+console.log(typeof ageInNumber)
+
+// But what if the number is like this in string 
+let experience ="15 years"
+
+let experienceInNumber= Number(experience)
+console.log(typeof experienceInNumber)
+console.log(experienceInNumber)
