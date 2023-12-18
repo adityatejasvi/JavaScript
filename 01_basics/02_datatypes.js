5bab801a1bb2cad507b247ee3f6e978b630954db
adityatejasvi <111077816+adityatejasvi@users.noreply.github.com>
2023-12-18 08:27:54 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+1) // we are using nodejs, not browser
+
+let name="Aditya"  //string
+let age=20         // int 
+let isPresent=true // boolean
+
+// Data Types
+// number , Range 2 to power 53
+//bigint
+//string =>"""
+//boolean =>true/false
+//null =>standalone value
+//undefined =>value not assigned
+//symbol =>to find uniqness (in React)
+// object
+
+console.log(typeof undefined)  // undefined
+console.log(typeof null)       // null
+
+
+// Read documentation from ECMAScript (tc39.es) and Mdn
