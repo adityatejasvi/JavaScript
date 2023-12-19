0ea63e545f78824717e5af3d40631058fad86260
adityatejasvi <111077816+adityatejasvi@users.noreply.github.com>
2023-12-19 05:32:51 +0000

    Operations Done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,3 +20,79 @@ let experience ="15 years"
 let experienceInNumber= Number(experience)
 console.log(typeof experienceInNumber)
 console.log(experienceInNumber)
+
+// Taking null
+let victory=null
+console.log(typeof null)
+
+let val=Number(victory)
+console.log(typeof val)
+console.log(val)
+
+// Taking boolean value 
+let attacked=true
+console.log(typeof attacked)
+
+let casualties=Number(attacked)
+console.log(typeof casualties)
+console.log(casualties)
+
+// Taking Undefined 
+let date=undefined
+console.log(typeof date)
+
+let dValue=Number(date)
+console.log(typeof dValue)
+console.log(dValue)
+
+// "33" => 33
+// "33abc"=>NaN (Not a Number)
+// true => 1; false=>0
+ 
+//If we convert type in boolean 
+// 1=>true; 0=>false
+// ""=>fasle
+// "Aditya"=>true
+
+
+// String 
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+// ***************************** Operations **********************************
+console.log("Starting Operation ")
+ let value=3
+ let negvalue= - value
+ console.log(negvalue)
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**3);
+//  console.log(2/3);
+//  console.log(2%3);
+
+// Concatination of two string
+let str1="Aditya"
+let str2=" Pratap"
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1" +2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(true)
+console.log(+true) // Not a good Practice
+console.log(+"")   // not a good Practice
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter);
+
+// Here pr-increment and post increment act same but we have to diiferentiate
+
+ 
