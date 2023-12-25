db1928b4af6962b79ab91ec4fded64f57db07834
Aditya Pratap Tejasvi <111077816+adityatejasvi@users.noreply.github.com>
2023-12-25 13:49:12 +0000

    Arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,14 @@
+// Array
+
+const arr=[1,2,3,4,5];
+const heros=["Batman","Perman","Superman","Superman"];
+const info=["Abhay",15,"Aradhana",13,"Aditya",14];
+console.log(arr);
+console.log(heros);
+console.log(info);
+console.log(arr[1]);
+// Here in Js we can keep different types of data in an array 
+// we can also increase the sixe of given array 
+// we can acess array element using index
+
+// Array Methods
